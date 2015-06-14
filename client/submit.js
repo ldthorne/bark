bfb5a3114bc86384d2ddd699ad988bf61e5fcd2a
Leon Thorne <ldthorne@brandeis.edu>
2015-06-14 19:10:48 -0400

    deleted typos in text

--- client/submit.js
+++ client/submit.js
@@ -1,12 +1,11 @@
 Template.submit.events({
   'submit .postsSubmitForm': function(event) {
     event.preventDefault();
-    var post = event.target.post.value; // get yak value
+    var post = event.target.post.value; // get post vote value
     // check if the value is empty
     if (post == "") {
       alert("You can’t insert empty post. Try to write something funny instead.");
     } else {
-      console.log("we got past else");
       Meteor.call('postInsert', post);
       Router.go('newsfeed');
     }
