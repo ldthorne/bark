4b2217fdae0c569c713d2aa9d6d1dd2eacc5c294
Jason Theoharis <jtheo@brandeis.edu>
2015-07-08 16:44:38 -0400

    added speech recognition to submit

--- client/submit.js
+++ client/submit.js
@@ -1,4 +1,5 @@
 
+
 Meteor.startup(function() {
     start = currentLocation();
 });
@@ -26,7 +27,13 @@ Template.submit.events({
     console.log(start);
     Session.set("currentLocation",start);
     });
+  },
+
+  'click #start_button': function(event){
+    startDictation(event);
   }
+
+
 });
 
 Template.submit.helpers({
@@ -58,5 +65,82 @@ function Point(x,y) {
   this.x = x;
   this.y = y;
 }
+
+
+
+
+/*
+  This code comes from this blog post by Amit Agarwal
+      http://ctrlq.org/code/19680-html5-web-speech-api
+*/
+
+  var final_transcript = '';
+  var recognizing = false;
+  
+  if ('webkitSpeechRecognition' in window) {
+    console.log("webkit is available!");
+    var recognition = new webkitSpeechRecognition();
+      recognition.continuous = true;
+      recognition.interimResults = true;
+ 
+      recognition.onstart = function() {
+        recognizing = true;
+      };
+ 
+      recognition.onerror = function(event) {
+        console.log(event.error);
+      };
+ 
+      recognition.onend = function() {
+        recognizing = false;
+      };
+ 
+      recognition.onresult = function(event) {
+      myevent = event;
+        var interim_transcript = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+        console.log("i="+i);
+          if (event.results[i][0].transcript.indexOf("stop") > -1){
+            console.log("woord");
+            recognition.stop()
+            return;
+          }
+          if(event.results[i].isFinal) {
+            final_transcript += capitalize(event.results[i][0].transcript.trim()) +".\n";
+        console.log('final events.results[i][0].transcript = '+ JSON.stringify(event.results[i][0].transcript));
+          } else {
+            interim_transcript += event.results[i][0].transcript;
+        console.log('interim events.results[i][0].transcript = '+ JSON.stringify(event.results[i][0].transcript));
+          }
+        }
+        //final_transcript = capitalize(final_transcript);
+       document.getElementById("post").value = linebreak(final_transcript);
+        document.getElementById("post").value = linebreak(interim_transcript);
+        
+      };
+  }
+ 
+  var two_line = /\n\n/g;
+  var one_line = /\n/g;
+  function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+  }
+ 
+  function capitalize(s) {
+    return s.replace(s.substr(0,1), function(m) { return m.toUpperCase(); });
+  }
+ 
+  function startDictation(event) {
+    if (recognizing) {
+      recognition.stop();
+      return;
+    }
+    final_transcript = '';
+    recognition.lang = 'en-US';
+    recognition.start();
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+  }
+  
 
 
