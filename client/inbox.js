9c458a83c111306140aa93be3be470de1cd49756
lukedorney <l36dorn@brandeis.edu>
2015-07-09 15:33:47 -0400

    updating mail again

--- client/inbox.js
+++ client/inbox.js
@@ -1,20 +1,26 @@
+
 Template.inbox.helpers({
    messagefunc: function(){return Messages.find({},{sort:{time: 1}});}
  });
 
-Template.messageform.events({
-	"submit #sendmessage": function(event){
-		event.preventDefault();
+// Template.messageblock.helpers({
+// 	postData: function(){return Posts.find(this.postId);}
+// });
+
+
+// Template.messageform.events({
+// 	"submit #sendmessage": function(event){
+// 		event.preventDefault();
 
 		
-		var timestamp = new Date();
-		var message= event.target.message.value;
-		var to_whom= event.target.to_whom.value;
-		var sender = Meteor.userId();
-		console.log(JSON.stringify(message));
+// 		var timestamp = new Date();
+// 		var message= event.target.message.value;
+// 		var to_whom= event.target.to_whom.value;
+// 		var sender = Meteor.userId();
+// 		console.log(JSON.stringify(message));
 
-		Messages.insert({timestamp:timestamp, message:message, to_whom:to_whom, sender:sender});
+// 		Messages.insert({timestamp:timestamp, message:message, to_whom:to_whom, sender:sender});
 
-		Router.go('/inbox');	
-	}
-})
+// 		Router.go('/inbox');	
+// 	}
+// })
