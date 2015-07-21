c3abcb55b32d3815243c9375337c24e983a312f0
Jason Theoharis <jtheo@brandeis.edu>
2015-07-21 14:11:01 -0400

    search bar on homepage and emoji comments

--- client/newsfeed.js
+++ client/newsfeed.js
@@ -5,6 +5,55 @@ audio = new Audio('audio/bark.wav');
 
 
 
+
+
+
+
+
+/*
+
+var options = {
+  keepHistory: 1000 * 60 * 5,
+  localSearch: true
+};
+var fields = ['packageName', 'description'];
+
+PackageSearch = new SearchSource('packages', fields, options);
+
+Template.searchResult.helpers({
+  getPackages: function() {
+    return PackageSearch.getData({
+      transform: function(matchText, regExp) {
+        return matchText.replace(regExp, "<b>$&</b>")
+      },
+      sort: {isoScore: -1}
+    });
+  },
+  
+  isLoading: function() {
+    return PackageSearch.getStatus().loading;
+  }
+});
+
+Template.searchResult.rendered = function() {
+  PackageSearch.search('');
+};
+
+Template.searchBox.events({
+  "keyup #search-box": _.throttle(function(e) {
+    var text = $(e.target).val().trim();
+    PackageSearch.search(text);
+  }, 200)
+});
+
+*/
+
+
+
+
+
+
+
 Template.newsfeed.helpers({
   posts: function() {
     return Posts.find({}, {sort: {submitted: -1}});
