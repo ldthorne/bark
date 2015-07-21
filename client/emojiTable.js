7b12a77af9dd7042cfd38d2fa53f7c63f6a4a08a
lukedorney <l36dorn@brandeis.edu>
2015-07-21 18:15:47 -0400

    fixed emoji layout, and added sorting

--- client/emojiTable.js
+++ client/emojiTable.js
@@ -1,6 +1,9 @@
+Session.set('emojiSelect', 'first');
+
 Template.emojiTable.helpers({
 	emojifunc: function(){
-		return Emojis.find({});
+		var selected = Session.get('emojiSelect')
+		return Emojis.find({section: selected});
 	},
 	selectedemoji: function(){
 		if(Session.get('emoji') !== "" && Session.get('emoji') != null){
@@ -19,5 +22,20 @@ Template.emojiTable.events({
 			$("#post").val($("#post").val()+this.code);
 			$("#comment").val($("#comment").val()+this.code);
 		}
+    },
+    'click #one': function(){
+    	Session.set('emojiSelect', "first");
+    },
+    'click #two': function(){
+    	Session.set('emojiSelect', "second");
+    },
+    'click #three': function(){
+    	Session.set('emojiSelect', "third");
+    },
+    'click #four': function(){
+    	Session.set('emojiSelect', "fourth");
+    },
+    'click #five': function(){
+    	Session.set('emojiSelect', "fifth");
     }
 })
