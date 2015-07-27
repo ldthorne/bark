e072ba117d73151474aadcbb71032798fa80ec6f
Jason Theoharis <jtheo@brandeis.edu>
2015-07-27 14:06:37 -0400

    time since for both posts and comments

--- client/comment.js
+++ client/comment.js
@@ -37,7 +37,13 @@ Template.commentForm.events({
 });
 
 Template.commentBloc.helpers({
-   ismyrow: function(){return Meteor.userId() == this.commenter}
+   ismyrow: function(){return Meteor.userId() == this.commenter},
+  
+    submitted:function(){
+    console.log(this.submitted);
+    return submittime(this.submitted);
+  }
+
 });
 
 
@@ -159,6 +165,51 @@ function removeComment(selectedId){
   Comments.remove(selectedId);
 }
 
+
+function submittime(submitted){
+var tTime=new Date(submitted);
+    var cTime=new Date();
+    var sinceMin=Math.round((cTime-tTime)/60000);
+    if(sinceMin==0)
+    {
+        var sinceSec=Math.round((cTime-tTime)/1000);
+        if(sinceSec<10)
+          var since='less than 10 seconds ago';
+        else if(sinceSec<20)
+          var since='less than 20 seconds ago';
+        else
+          var since='half a minute ago';
+    }
+    else if(sinceMin==1)
+    {
+        var sinceSec=Math.round((cTime-tTime)/1000);
+        if(sinceSec==30)
+          var since='half a minute ago';
+        else if(sinceSec<60)
+          var since='less than a minute ago';
+        else
+          var since='1 minute ago';
+    }
+    else if(sinceMin<45)
+        var since=sinceMin+' minutes ago';
+    else if(sinceMin>44&&sinceMin<60)
+        var since='about 1 hour ago';
+    else if(sinceMin<1440){
+        var sinceHr=Math.round(sinceMin/60);
+    if(sinceHr==1)
+      var since='about 1 hour ago';
+    else
+      var since='about '+sinceHr+' hours ago';
+    }
+    else if(sinceMin>1439&&sinceMin<2880)
+        var since='1 day ago';
+    else
+    {
+        var sinceDay=Math.round(sinceMin/1440);
+        var since=sinceDay+' days ago';
+    }
+    return since;
+}
 
 
 
--- client/newsfeed.js
+++ client/newsfeed.js
@@ -9,7 +9,6 @@ Template.newsfeed.helpers({
   posts: function() {
     return Posts.find({}, {sort: Session.get('postsSort')});
   }
-
 });
 
 Template.postInfo.helpers({
@@ -25,7 +24,12 @@ Template.postInfo.helpers({
     } else {
       return "comments";
     }
+  },
+  submitted:function(){
+console.log(this.submitted);
+    return submittime(this.submitted);
   }
+
 });
 
 Template.postInfo.events({
@@ -244,6 +248,53 @@ function removePost(selectedId){
   });
   Posts.remove(selectedId);
 }
+
+
+function submittime(submitted){
+var tTime=new Date(submitted);
+    var cTime=new Date();
+    var sinceMin=Math.round((cTime-tTime)/60000);
+    if(sinceMin==0)
+    {
+        var sinceSec=Math.round((cTime-tTime)/1000);
+        if(sinceSec<10)
+          var since='less than 10 seconds ago';
+        else if(sinceSec<20)
+          var since='less than 20 seconds ago';
+        else
+          var since='half a minute ago';
+    }
+    else if(sinceMin==1)
+    {
+        var sinceSec=Math.round((cTime-tTime)/1000);
+        if(sinceSec==30)
+          var since='half a minute ago';
+        else if(sinceSec<60)
+          var since='less than a minute ago';
+        else
+          var since='1 minute ago';
+    }
+    else if(sinceMin<45)
+        var since=sinceMin+' minutes ago';
+    else if(sinceMin>44&&sinceMin<60)
+        var since='about 1 hour ago';
+    else if(sinceMin<1440){
+        var sinceHr=Math.round(sinceMin/60);
+    if(sinceHr==1)
+      var since='about 1 hour ago';
+    else
+      var since='about '+sinceHr+' hours ago';
+    }
+    else if(sinceMin>1439&&sinceMin<2880)
+        var since='1 day ago';
+    else
+    {
+        var sinceDay=Math.round(sinceMin/1440);
+        var since=sinceDay+' days ago';
+    }
+    return since;
+}
+
 
 
 function FindNext() {
