149f164ad111469cc0c390e86cb99fd8c8342399
Jason Theoharis <jtheo@brandeis.edu>
2015-07-28 13:13:58 -0400

    fix post layout for mobile

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -20,284 +20,32 @@ Template.layout.rendered = function(){
 
 }
 
-Template.layout.events({
-	// 'click #micbutton': function(event) {
-	// 	startMic(event);
-	// }
-  'click #micbutton': function(event){
-    console.log("starting dictation");
-    if ($('#speaker-turn').html()=="start") {
-      $('#speaker-turn').html('stop');
-      startDictation(event);
-    } else {
-      $('#speaker-turn').html('start');
-      recognition.stop();
-      recognizing=false;
-      /*
-      var user_utterance = final_span.innerHTML;
-      user_utterance = user_utterance.substring(0,user_utterance.indexOf(' --'));
-      handle_user_input(user_utterance);
-      var response = "You just said "+user_utterance;
-      say(response);
-      handle_user_input(user_utterance.toLowerCase());
-      */
-    }
 
-  }
 
-})
 
-function say(text){
-  if (pauseTimeout){
-    console.log("clearing pause Timeout");
-    window.clearTimeout(pauseTimeout);
-    pauseTimeout = null;
-  }
-  recognition.onend = function(){console.log("recognition is stopped"); sayitnow(text);};
-  console.log("stopping recognition");
-  recognition.stop();
-}
-
-var pauseTimeout = null;
-
-function sayitnow(text){
-  console.log("starting to talk");
-  var msg = new SpeechSynthesisUtterance(text+".  Ready");
-  msg.onend = function(event){
-    console.log("speech over"+ "said '"+msg.text+"' .... starting recognition!"); 
-    final_transcript = '';
-    recognition.start();
-    pauseTimeout = window.setTimeout(function(){handle_user_input("next")},5000);
-  };
-  window.speechSynthesis.speak(msg);
-}
-
-var i = 0;
-
-function handle_user_input(u){
-  var responded = false;
-  console.log("hui: "+u+" i="+i);
-  u = u.toLowerCase();
-  if ((u.indexOf("next")>-1) || (u.indexOf("start")>-1)) {
-    if (i < numbers.length){
-      console.log("saying "+i+"th number");
-      say(numbers[i++]);
-    } else {
-      say("there are no more numbers")
-    };
-
-    responded = true;
-  } else if (u.indexOf("reset")> -1) {
-    i=0;
-    say("OK!  Resetting!");
-    responded = true;
-  } else if (u.indexOf("repeat")>-1){
-    say(numbers[i-1]);  
-    responded = true;
-  }
-  
-}
-
-  // var final_transcript = '';
-  // var recognizing = false;
-  // var dictating = false;
-  
-var final_transcript = '';
-var recognizing = false;
-var numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
-var position = 0;
-
-if ('webkitSpeechRecognition' in window) {
-  console.log("webkit is available!");
-  var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-
-    recognition.onstart = function() {
-      recognizing = true;
-    };
-
-    recognition.onerror = function(event) {
-      console.log(event.error);
-    };
-
-    recognition.onend = function() {
-      recognizing = false;
-    };
-
-    recognition.onresult = function(event) {
-    myevent = event;
-      var interim_transcript = '';
-      for (var i = event.resultIndex; i < event.results.length; ++i) {
-      console.log("i="+i+" words="+words);
-    var words = event.results[i][0].transcript;
-    console.log("ready to handle input: '"+words+"'");
-    if (words) handle_user_input(words);
-    //handle_user_input(words);
-    if (words.includes("stop dictation")) {
-      recognition.stop();
-    } else if (words.includes("read it back")){
-      var msg = new SpeechSynthesisUtterance(words);
-      window.speechSynthesis.speak(msg);
-    }
-        if (event.results[i].isFinal) {
-          console.log("final result is |"+event.results[i][0].transcript.trim()+"|");
-          final_transcript += 
-      capitalize(event.results[i][0].transcript.trim()) +" -- " + Math.round(100*event.results[i][0].confidence)+"%\n";
-      console.log('final events.results[i][0].transcript = '+ JSON.stringify(event.results[i][0].transcript));
-        } else {
-          interim_transcript += Math.round(100*event.results[i][0].confidence) + "%: "+ event.results[i][0].transcript+"<br>";
-      console.log('interim events.results[i][0].transcript = '+ JSON.stringify(event.results[i][0].transcript));
-        }
-      }
-      //final_transcript = capitalize(final_transcript);
-    //console.log("ready to handle input: '"+final_transcript+"'");
-    //handle_user_input(final_transcript);
+// function readPosts(){
+//     allPosts = Posts.find().fetch();
+//     console.log(allPosts);
     
-      final_span.innerHTML = linebreak(final_transcript);
-      interim_span.innerHTML = linebreak(interim_transcript);
-    
-    
-    };
-}  
-
-var two_line = /\n\n/g;
-var one_line = /\n/g;
-
-function linebreak(s) {
-  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
-}
-
-function capitalize(s) {
-  return s.replace(s.substr(0,1), function(m) { return m.toUpperCase(); });
-}
-
-function startDictation(event) {
-  console.log("starting dictation");
-
-  if (recognizing) {
-    //recognition.stop();
-    return;
-  }
-  final_transcript = '';
-  recognition.lang = 'en-US';
-  
-  final_span.innerHTML = '';
-  interim_span.innerHTML = '';
-  recognition.start();
-}
-
-  // if ('webkitSpeechRecognition' in window) {
-  //   var recognition = new webkitSpeechRecognition();
-  //     recognition.continuous = true;
-  //     recognition.interimResults = true;
- 
-  //     recognition.onstart = function() {
-  //       recognizing = true;
-  //     };
- 
-  //     recognition.onerror = function(event) {
-  //       console.log(event.error);
-  //     };
- 
-  //     recognition.onend = function() {
-  //       recognizing = false;
-  //     };
- 
-  //     recognition.onresult = function(event) {
-
-  //     myevent = event;
-  //       var interim_transcript = '';
-
-  //       for (var i = event.resultIndex; i < event.results.length; ++i) {
-  //       	console.log("recognizing" + event.results[i][0].transcript);
-  //       	//$("#test").html(interim_transcript);
-  //         if (event.results[i][0].transcript.indexOf("stop") > -1){
-  //           recognition.stop();
-  //           return;
-  //         } if((event.results[i][0].transcript.indexOf("submit") > -1) || (event.results[i][0].transcript.indexOf("post") > -1)){
-  //         	recognition.stop();
-  //         	Router.go("submit");
-  //         } if ((event.results[i][0].transcript.indexOf("inbox") > -1) || (event.results[i][0].transcript.indexOf("messages") > -1)) {
-  //         	recognition.stop();
-  //         	Router.go("inbox");
-  //         } if ((event.results[i][0].transcript.indexOf("newsfeed") > -1) || (event.results[i][0].transcript.indexOf("home") > -1) || (event.results[i][0].transcript.indexOf("news feed") > -1)) {
-  //         	recognition.stop();
-  //         	Router.go("newsfeed");
-
-  //         } if (Router.current().route.getName() == "submit"){
-  //         		if((event.results[i][0].transcript.indexOf("dictate") > -1) || (event.results[i][0].transcript.indexOf("dictation") > -1)){
-  //         			dictating=true;
-          			
-  //         		}
-  //         }
-  //         if((Router.current().route.getName() == "newsfeed")){
-  //               if((event.results[i][0].transcript.indexOf("read") > -1)){
-  //                   if(!alreadyRead){
-  //                     readPosts();
-  //                     alreadyRead = true;
-  //                   }
-  //                   if(
-  //                     (event.results[i][0].transcript.indexOf("upvote") > -1) || 
-  //                     (event.results[i][0].transcript.indexOf("a boat") > -1) || 
-  //                     (event.results[i][0].transcript.indexOf("like") > -1) || 
-  //                     (event.results[i][0].transcript.indexOf("upload") > -1) || 
-  //                     (event.results[i][0].transcript.indexOf("up vote") > -1)){
-  //                       console.log("done");
-  //                       recognition.stop();
-  //                   }
-  //               }
-  //         } 
-  //         // if (Router.current().route.getName() == "newsfeed"){
-
-  //         // }
-  //         if(event.results[i].isFinal) {
-  //           final_transcript += event.results[i][0].transcript.trim() +".\n";
-  //           console.log("final :");
-  //           if(dictating){
-  //           	dictating=false;
-  //           	 $("#post").val(final_transcript);
-  //           	} else{
-  //           		 $("#test").html("bark! thinks you said: " + final_transcript);
-  //           	}
-            
-  //         } else {
-  //           interim_transcript += event.results[i][0].transcript;
-  //           console.log("not final");
-  //           if (dictating){
-  //            	$("#post").val(interim_transcript);
-  //           } else{
-  //           	$("#test").html("bark! thinks you said: " + interim_transcript);
-  //           }
-  //         }
-  //       }
-        
-  //     };
-  // }
-
-function readPosts(){
-    allPosts = Posts.find().fetch();
-    console.log(allPosts);
-    
-    var posts = _.pluck(allPosts, 'post');
-    var postsId = _.pluck(allPosts, '_id')
-    var reversePosts = posts.reverse()
-    var reversePostsId = postsId.reverse()
-    var zipped = _.zip(reversePosts, reversePostsId)
-    _.each(zipped, function(pair){
-      // console.log(pair[0]);
-      // console.log(pair[1]);
-      Session.set("currentPost", pair[1])
-      console.log(Session.get("currentPost"));
-      var msg = new SpeechSynthesisUtterance(pair[0]);
-      msg.onend = function(){
-        playAudio();      
-      }      
-      window.speechSynthesis.speak(msg);
-    })
+//     var posts = _.pluck(allPosts, 'post');
+//     var postsId = _.pluck(allPosts, '_id')
+//     var reversePosts = posts.reverse()
+//     var reversePostsId = postsId.reverse()
+//     var zipped = _.zip(reversePosts, reversePostsId)
+//     _.each(zipped, function(pair){
+//       // console.log(pair[0]);
+//       // console.log(pair[1]);
+//       Session.set("currentPost", pair[1])
+//       console.log(Session.get("currentPost"));
+//       var msg = new SpeechSynthesisUtterance(pair[0]);
+//       msg.onend = function(){
+//         playAudio();      
+//       }      
+//       window.speechSynthesis.speak(msg);
+//     })
 
 
-}
+// }
 
 // function readPosts(){
 //     allPosts = Posts.find().fetch();
--- client/newsfeed.js
+++ client/newsfeed.js
@@ -4,6 +4,14 @@ theVoice=null;
 audio = new Audio('audio/bark.wav');
 Session.set('postsSort', {submitted: -1});
 
+Template.newsfeed.rendered = function(){
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+  putPosts();
+
+};
+
 
 Template.newsfeed.helpers({
   posts: function() {
@@ -26,7 +34,7 @@ Template.postInfo.helpers({
     }
   },
   submitted:function(){
-console.log(this.submitted);
+// console.log(this.submitted);
     return submittime(this.submitted);
   }
 
@@ -53,9 +61,35 @@ Template.postInfo.events({
 function playAudio(){
   audio.play();
 }
+var clicked = false;
 
 Template.newsfeed.events({
-    
+  'click #micbutton': function(event){
+    clicked = !clicked;
+    console.log("clicked")
+
+    _.each(postArray, function(post){
+      console.log(post.post);
+    });
+    if (clicked) {
+      console.log("yes")
+      startDictation(event);
+    } else {
+      console.log("no")
+      recognition.stop();
+      recognizing=false;
+      /*
+      var user_utterance = final_span.innerHTML;
+      user_utterance = user_utterance.substring(0,user_utterance.indexOf(' --'));
+      handle_user_input(user_utterance);
+      var response = "You just said "+user_utterance;
+      say(response);
+      handle_user_input(user_utterance.toLowerCase());
+      */
+    }
+
+  },
+
     'click': function(){
       var postId = this._id;
       Session.set('post', postId);
@@ -168,26 +202,156 @@ Template.newsfeed.events({
       })
     },
 
-  'click #timeUp':function(){
+    'change #selectSort' : function(){
+      var val = $("#selectSort option:selected").text();
+      console.log(val);
+      if(val == "Newest"){
         Session.set('postsSort', {submitted: -1});
-               
-    },
-
-    'click #timeDown':function(){
+      } else if (val == "Oldest"){
         Session.set('postsSort', {submitted: 1});
-       
-    },
-    'click #voteUp':function(){
+      } else if (val == "Highest Score"){
         Session.set('postsSort', {score: -1});
-             
-    },
-
-    'click #voteDown':function(){
+      } else if (val == "Lowest Score"){
         Session.set('postsSort', {score: 1});
-        
+      }
     },
-
 });
+
+function say(text){
+  if (pauseTimeout){
+    console.log("clearing pause Timeout");
+    window.clearTimeout(pauseTimeout);
+    pauseTimeout = null;
+  }
+  recognition.onend = function(){console.log("recognition is stopped"); sayitnow(text);};
+  console.log("stopping recognition");
+  recognition.stop();
+}
+var pauseTimeout = null;
+
+  function sayitnow(text){
+  console.log("\n\nSPEAKING: "+text+"\n\n");
+  var msg = new SpeechSynthesisUtterance(text+".  Ready");
+  msg.onend = function(event){
+    console.log("speech over"+ "said '"+msg.text+"'\n\n RECOGNIZING\n\n"); 
+    final_transcript = '';
+    recognition.start();
+    //pauseTimeout = window.setTimeout(function(){handle_user_input("next")},5000);
+  };
+  window.speechSynthesis.speak(msg);
+}
+
+var i = 0;
+
+function handle_user_input(u){
+  var responded = false;
+  console.log("    hui: "+u+" i="+i);
+  u = u.toLowerCase();
+  if ((u.indexOf("next")>-1) || (u.indexOf("start")>-1)) {
+    if (i < numbers.length){
+      console.log("   hui: saying "+i+"th number");
+      say(numbers[i++]);
+    } else {
+      say("there are no more numbers")
+    };
+
+    responded = true;
+  } else if (u.indexOf("reset")> -1) {
+    i=0;
+    say("OK!  Resetting!");
+    responded = true;
+  } else if (u.indexOf("repeat")>-1){
+    say(numbers[i-1]);  
+    responded = true;
+  }
+  
+}
+
+var final_transcript = '';
+var recognizing = false;
+
+function putPosts(){
+  allPosts = Posts.find().fetch();
+  console.log(allPosts);
+}
+
+
+var position = 0;
+
+if ('webkitSpeechRecognition' in window) {
+  console.log("webkit is available!");
+  var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onstart = function() {
+      recognizing = true;
+    };
+
+    recognition.onerror = function(event) {
+      console.log(event.error);
+    };
+
+    recognition.onend = function() {
+      recognizing = false;
+    };
+
+    recognition.onresult = function(event) {
+    myevent = event;
+      var interim_transcript = '';
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+      console.log("   onResult: i="+i+" words="+words);
+    var words = event.results[i][0].transcript;
+    console.log("    onResult: ready to handle input: '"+words+"'");
+    //if (words) handle_user_input(words);
+    //handle_user_input(words);
+        if (event.results[i].isFinal) {
+          console.log("    onResult: final result is |"+event.results[i][0].transcript.trim()+"|");
+          final_transcript += 
+      capitalize(event.results[i][0].transcript.trim()) +" -- " + Math.round(100*event.results[i][0].confidence)+"%\n";
+      console.log('    onResult: final events.results[i][0].transcript = '+ JSON.stringify(event.results[i][0].transcript));
+        } else {
+          interim_transcript += Math.round(100*event.results[i][0].confidence) + "%: "+ event.results[i][0].transcript+"<br>";
+      console.log('    onResult: interim events.results[i][0].transcript = '+ JSON.stringify(event.results[i][0].transcript));
+        }
+      }
+      //final_transcript = capitalize(final_transcript);
+    console.log("ready to handle input: '"+final_transcript+"'");
+    handle_user_input(final_transcript);
+    
+      final_span.innerHTML = linebreak(final_transcript);
+      interim_span.innerHTML = linebreak(interim_transcript);
+    
+    
+    };
+}
+
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+function linebreak(s) {
+  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+function capitalize(s) {
+  return s.replace(s.substr(0,1), function(m) { return m.toUpperCase(); });
+}
+
+final_span = '';
+interim_span='';
+
+function startDictation(event) {
+  if (recognizing) {
+    //recognition.stop();
+    return;
+  }
+  final_transcript = '';
+  recognition.lang = 'en-US';
+  
+  final_span.innerHTML = '';
+  interim_span.innerHTML = '';
+  recognition.start();
+}
 
 function checkVotes(selected){
     var p = Posts.findOne({_id:selected._id});
