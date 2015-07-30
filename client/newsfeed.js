9a6fb6845247193a0ce1a43add5701e0b2d8ada0
Leon Thorne <ldthorne@brandeis.edu>
2015-07-30 17:23:35 -0400

    commented out the search functions

--- client/newsfeed.js
+++ client/newsfeed.js
@@ -405,25 +405,25 @@ function submittime(submitted){
       return since;
 }
 
-function search(){
-  var s = document.querySelector('input[type="search"]'),
-    p = document.querySelector('p'),
-    find = function(){
-        var words = p.innerText.split(' '),
-            i = words.length,
-            word = '';
-
-        while(--i) {
-            word = words[i];
-            if(word.toLowerCase() == s.value.toLowerCase()){
-                words[i] = '<span class="highlight">' + word + "</span>";
-            }
-            else{
-            }
-        }
-        p.innerHTML = words.join(' ');
-    }
-
-s.addEventListener('keydown', find , false);
-s.addEventListener('keyup', find , false);
-}
+// function search(){
+//   var s = document.querySelector('input[type="search"]'),
+//     p = document.querySelector('p'),
+//     find = function(){
+//         var words = p.innerText.split(' '),
+//             i = words.length,
+//             word = '';
+//
+//         while(--i) {
+//             word = words[i];
+//             if(word.toLowerCase() == s.value.toLowerCase()){
+//                 words[i] = '<span class="highlight">' + word + "</span>";
+//             }
+//             else{
+//             }
+//         }
+//         p.innerHTML = words.join(' ');
+//     }
+//
+// s.addEventListener('keydown', find , false);
+// s.addEventListener('keyup', find , false);
+// }
